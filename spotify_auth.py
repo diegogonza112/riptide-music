c860d5379348c54d39c595d919ae521476b4c75c
diegogonza112 <diego.gonzalezdelgado@mail.utoronto.ca>
2022-01-16 22:17:59 -0500

    n/a

--- spotify_auth.py
+++ spotify_auth.py
@@ -3,10 +3,11 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import pandas as pd
 
-SPOTIPY_CLIENT_ID = "ca227eab01e349dfac9691f88f7c9d80"
-SPOTIPY_CLIENT_SECRET = "119a23b3da1a4fcf82cf8c9e2936a677"
+SPOTIPY_CLIENT_ID = "648d2baba64542109c3b9eb8d9525798"
+SPOTIPY_CLIENT_SECRET = "31858e4f138a414aa1874fe5488f76e7"
 SPOTIPY_REDIRECT_URI = "https://hidden-castle-24851.herokuapp.com/"
 SCOPE = "user-top-read"
+
 
 def read():
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
