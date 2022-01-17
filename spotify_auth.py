2d6678de803f6f33129dae009721a09d2d9ff435
diegogonza112 <diego.gonzalezdelgado@mail.utoronto.ca>
2022-01-17 15:25:10 -0500

    n/a

--- spotify_auth.py
+++ spotify_auth.py
@@ -1,24 +1,26 @@
+import json
+
 import requests
+import spotipy.util
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import pandas as pd
+import base64
 
 SPOTIPY_CLIENT_ID = "648d2baba64542109c3b9eb8d9525798"
 SPOTIPY_CLIENT_SECRET = "31858e4f138a414aa1874fe5488f76e7"
-SPOTIPY_REDIRECT_URI = "https://hidden-castle-24851.herokuapp.com/"
-SCOPE = "user-top-read"
-TOKEN = "BQCBv5qY6xax0kNy28mqACbwqmQulDNdqoPWse4MnqagXgBimLyC0rn0zvgwbUEomwz" \
-        "PTi-6n2N9nY8vpLKpmYc_jTufuNrlkPWWDcCc_Jw82MWPcKPC0Q3AKJJLsbx6gHhPtBDE" \
-        "re2pl8o"
+SPOTIPY_REDIRECT_URI = "https://hidden-castle-24851.herokuapp.com/login"
+SCOPE = "user-read-email"
 
 
 def user():
-    sp = spotipy.Spotify(TOKEN,
-                         auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
-                                                   client_secret=
-                                                   SPOTIPY_CLIENT_SECRET,
-                                                   redirect_uri=
-                                                   SPOTIPY_REDIRECT_URI,
-                                                   scope=SCOPE
-                                                   ))
+    sp = spotipy.Spotify(
+        client_credentials_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
+                                                client_secret=
+                                                SPOTIPY_CLIENT_SECRET,
+                                                redirect_uri=
+                                                SPOTIPY_REDIRECT_URI,
+                                                scope=SCOPE
+                                                ))
     return sp.me()['display_name']
+
+print(user())
